be55b15b70878e265525c1f85925c75a33de8ce1
Eloísa Sandoval <128868290+elosandoval18@users.noreply.github.com>
2025-07-30 03:10:18 +0000

    Challenge 2nd solsution

--- src/Ch01/01_04/CodeDemo.cpp
+++ src/Ch01/01_04/CodeDemo.cpp
@@ -6,10 +6,10 @@
 #include <string>
 
 int main(){
-    std::string str;
-    std::cout << "Enter your name: " << std::flush;
-    std::cin >> str;
-    std::cout << "Nice to meet you, " << str << "!" << std::endl;
+    std::string str; //string variable declaration
+    std::cout << "Enter your name: " << std::flush; //flush is used to make sure that this line of code has been completely sent to the terminal before the next line shows 
+    std::cin >> str; //capturing input into str variable
+    std::cout << "Nice to meet you, " << str << "!" << std::endl; //to concatenate elements.
 
     std::cout << std::endl << std::endl;
     return (0);
