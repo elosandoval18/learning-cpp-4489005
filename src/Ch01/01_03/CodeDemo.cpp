f7e0497d0e0a91d681cb3f4ae926244975fd8acb
Eloísa Sandoval <128868290+elosandoval18@users.noreply.github.com>
2025-07-30 02:55:33 +0000

    Challenge solution

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -2,13 +2,20 @@
 // Challenge 01_03
 // Console Interaction, by Eduardo Corpeño 
 
+
+// Ask for the user's name and reply by printing a message addresing the user's name
 #include <iostream>
 #include <string>
 
 int main(){
-    std::string str;
-    std::cin >> str;
+    std::string str("Type your username");
     std::cout << str;
+    std::cout << std::endl;
+
+    std::cin >> str; // This function only works for single words. For chains of text you need another function.
+    
+    std::string greet("Thanks");
+    std::cout <<greet;
 
     std::cout << std::endl << std::endl;
     return (0);
