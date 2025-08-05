23db9fb54c6a64bff9a5b66f200e083d5c1e6539
Eloísa Sandoval <128868290+elosandoval18@users.noreply.github.com>
2025-08-05 02:37:41 +0000

    Activity chapter 2

--- src/Ch02/02_03b/CodeDemo.cpp
+++ src/Ch02/02_03b/CodeDemo.cpp
@@ -4,9 +4,17 @@
 
 #include <iostream>
 
+int a, b = 5; //This two variables are at the same level of indentation as the main function. This means they are GLOBAL variables which are accessible by any part of the code. Memory for global variables is manages statically by the compiler. They are allocated in the data section of memory and once the program ends, memory is freed. 
+
+// In the other hand local variables are declarated inside a function, when the function is done then the variables are deleted and the memory is freed.
+
 int main(){
-    std::cout << "Hi There!" << std::endl;
+    bool my_flag;
+    a = 7;
+    my_flag = false;
     
-    std::cout << std::endl << std::endl;
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    std::cout << "Flag = " << my_flag << std::endl;
     return (0);
 }
